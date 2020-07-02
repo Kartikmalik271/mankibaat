e19b9dd90b3dda720b69812da6883bfc3002d21d
kartik malik <kartikmalik271@gmail.com>
2020-07-02 14:57:16 +0530

    Updates

---
+++ precache-manifest.8929d02c832f9cfa72880ac3ec683099.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95a9679e3bb769732112757efadb4b5",
+    "url": "/mankibaat/index.html"
+  },
+  {
+    "revision": "651e444961494bdedd06",
+    "url": "/mankibaat/static/css/main.6a7a0f87.chunk.css"
+  },
+  {
+    "revision": "02b3b9296ee518627691",
+    "url": "/mankibaat/static/js/2.05c1fa23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mankibaat/static/js/2.05c1fa23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651e444961494bdedd06",
+    "url": "/mankibaat/static/js/main.ff5ea491.chunk.js"
+  },
+  {
+    "revision": "f81d917f11f68ebc7da4",
+    "url": "/mankibaat/static/js/runtime-main.b1e47926.js"
+  },
+  {
+    "revision": "d67d55116d4595d3946a318ed9755e03",
+    "url": "/mankibaat/static/media/blogs.d67d5511.png"
+  },
+  {
+    "revision": "be785b905e3256e666f9549d28878a32",
+    "url": "/mankibaat/static/media/contact.be785b90.png"
+  },
+  {
+    "revision": "fa335674f11cec797378d85b647dd4f2",
+    "url": "/mankibaat/static/media/mkbbg.fa335674.png"
+  },
+  {
+    "revision": "b5f150bc4672977aaf26b39568206b67",
+    "url": "/mankibaat/static/media/service.b5f150bc.png"
+  }
+]);
